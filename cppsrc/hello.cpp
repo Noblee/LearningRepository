44e29e9cd611d47a84ea33f707bfb05be14b6c7f
AaronWang <AaronWang1129@gmail.com>
2021-02-06 17:52:49 +0800

    add cpp support

---
+++ cppsrc/hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Noble on 2021/2/6.
+//
+
+#include "hello.h"
+#include "iostream"
+using namespace std;
+int main() {
+    cout << "hello world";
+}
---
+++ cppsrc/hello.h
@@ -0,0 +1,14 @@
+//
+// Created by Noble on 2021/2/6.
+//
+
+#ifndef LEARNINGREPOSITORY_HELLO_H
+#define LEARNINGREPOSITORY_HELLO_H
+
+
+class hello {
+
+};
+
+
+#endif //LEARNINGREPOSITORY_HELLO_H
